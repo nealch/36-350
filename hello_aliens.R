0727a3f936226c4dcfd6a9e8095472756e528894
nealch <33407295+nealch@users.noreply.github.com>
2017-12-10 20:12:03 -0500

    Create hello_aliens.R

---
+++ hello_aliens.R
@@ -0,0 +1,16 @@
+cat("Hello Aliens,
+
+If you're reading this message, it means you have encountered humanity. On behalf of all the humans, let me welcome you to our planet.
+You have much to learn about how our species lives. On that note, I will share two general themes about us:
+
+    1) We are driven by self interest
+    2) We keep moving forward
+
+The first theme is the weakness of humanity. Although we all pretend it is a just world and that we want what is best
+for everyone, the truth is it is not a just world and we want what's best for ourselves. Due to this, you will find competing interests
+in every corner of earth. 
+
+The second theme is the strength of humanity. No matter how severe the obstacle, humans will keep trying to find a way through. This means
+you will find inspiring individuals that do their best to defy factors beyond their control everywhere.
+
+With humanity, you can chose to see the cup as half empty or half full. What about yourselves?")
